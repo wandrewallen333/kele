20e2b7f90adcee6e0835f3e88e146cdb847ffd99
Andrew Allen <w.andrewallen@gmail.com>
2018-01-05 10:59:35 -0600

    Some minor changes

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,7 +2,6 @@ class Kele
   include HTTParty
   def initialize(email, password)
     @options = { query: { email: email, password: password } }
-    response = self.class.post("https://www.bloc.io/api/v1", @options)
-    @auth_token = response["auth_token"]
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", @options)
   end
 end
