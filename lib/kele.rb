c61c61f3c41c62cda1cb65579f414a42f10a979b
Andrew Allen <w.andrewallen@gmail.com>
2018-01-05 11:26:01 -0600

    Retrieve Users 3: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,16 @@
+require 'rubygems'
+require 'httparty'
+require 'json'
 class Kele
   include HTTParty
   def initialize(email, password)
     @options = { query: { email: email, password: password } }
     response = self.class.post("https://www.bloc.io/api/v1/sessions", @options)
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 end
