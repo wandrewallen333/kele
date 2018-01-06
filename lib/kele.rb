716815b3620c07a2f8d984955918f171365e79a7
Andrew Allen <w.andrewallen@gmail.com>
2018-01-06 13:45:51 -0600

    Checkpoint Submission 7: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -40,7 +40,22 @@ class Kele
       if response.success?
           puts "message sent!"
       end
-  end      
+  end
+
+  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+    response = self.class.post('https://www.bloc.io/api/v1/checkpoint_submissions',
+      body: {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      },
+      headers: {"authorization" => @auth_token})
+    JSON.parse(response.body)
+  end
+
+
 
 
 
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,6 +1,6 @@
 module Roadmap
 
-  def get_roadmap
+  def get_roadmap(roadmap_id)
     response = self.class.get('https://www.bloc.io/api/v1/roadmaps/37', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
